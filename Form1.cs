2c1b31831509e875ba221de8dc0ac0d5cdd030f1
LuluTians <50853232+LuluTians@users.noreply.github.com>
2019-11-11 21:07:50 +1300

    new

---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double a = 0;   //记录计算结果
+        double b = 0;   //记录第一个操作数
+        bool c = false;  //当按下“+”，“-”，“*”，“/”等计算符号时将c设为true，
+                         //因为按下计算符号后，再按数字时，应当将原有数字清零。
+
+        string d;   //记录计算符号
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+            if(c==true )
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "0";
+            if(d == "/")  //之前按下/号
+            {
+                textBox1.Clear();   //清空文本框
+                //下面弹出警告，请自己尝试下
+                MessageBox.Show("除数不能为零", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "4";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (c == true)
+            {
+                textBox1.Text = "";
+                c = false;
+            }
+            textBox1.Text += "9";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "+";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "-";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "*";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "/";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "x2";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "sqrt";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "log";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            c = true;
+            b = double.Parse(textBox1.Text);
+            d = "ln";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            switch (d)
+            {
+                case "+": a = b + double.Parse(textBox1.Text); break;
+                case "-": a = b - double.Parse(textBox1.Text); break;
+                case "*": a = b * double.Parse(textBox1.Text); break;
+                case "/": a = b / double.Parse(textBox1.Text); break;
+                case "x2": a = b * double.Parse(textBox1.Text); break;
+                case "sqrt": a = Math.Sqrt(b); break;
+                case "log": a = Math.Log(double.Parse(textBox1.Text),b); break;
+                case "ln": a = Math.Log(b, Math.E); break;
+            }
+            textBox1.Text = a + "";
+            c = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
